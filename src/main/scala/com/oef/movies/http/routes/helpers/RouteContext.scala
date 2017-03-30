3278cd178ab845396079846fb4ca8186e7b89b60
Oleg Efrem <oleg.efrem@yahoo.com>
2017-03-30 16:30:51 +0100

    Add custom rejection handler

--- src/main/scala/com/oef/movies/http/routes/helpers/RouteContext.scala
+++ src/main/scala/com/oef/movies/http/routes/helpers/RouteContext.scala
@@ -2,11 +2,10 @@ package com.oef.movies.http.routes.helpers
 
 import akka.event.LoggingAdapter
 import akka.stream.ActorMaterializer
-import com.oef.movies.utils.{ Config, Protocol }
-
+import com.oef.movies.utils.Protocol
 import scala.concurrent.ExecutionContext
 
-trait RouteTemplate extends Protocol with RejectionHandling {
+trait RouteContext extends Protocol {
   protected implicit def executor: ExecutionContext
 
   protected implicit def materializer: ActorMaterializer
